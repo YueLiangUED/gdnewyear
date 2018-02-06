cdc2a8f85dd27b38d5a7db358eb61e86745230d6
wangbiaozy <56565283@qq.com>
2018-02-06 18:07:25 +0800

    1

--- js/index.js
+++ js/index.js
@@ -141,7 +141,7 @@ $(function () {
                 sayShow(say13,say13Show);
                 break;
         }
-        if(step <= -31060){
+        if(step <= -31070){
             $('#clockBtn').off();
             $('#clockBtn').remove();
             return;
